bb225cd77eed0fb90695842e6abe16cd624bea3a
p9tno <plahotin_s@mail.ru>
2023-05-25 14:54:09 +0300

    start about page

--- js/slider.js
+++ js/slider.js
@@ -53,4 +53,36 @@ $(document).ready(function() {
         }
     });
 
+    const ourteam = new Swiper('.swiper_ourteam_js', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        speed: 500,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+        },
+
+        navigation: {
+            nextEl: '.icon_arrow_right',
+            prevEl: '.icon_arrow_left',
+        },
+        pagination: {
+            el: '.dotted_ourteam_mobile_js',
+            clickable: true,
+        },
+
+        breakpoints: {
+            768: {
+                spaceBetween: 20,
+                slidesPerView: 3,
+
+                pagination: {
+                    el: '.dotted_ourteam_js',
+                    clickable: true,
+                },
+            },
+
+        }
+    });
+
 });
